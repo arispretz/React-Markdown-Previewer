dbdb597ac28882fbae4f926b6c4bbf644c456ff0
arispretz <117314460+arispretz@users.noreply.github.com>
2024-06-13 19:34:09 -0300

    Update index.jsx

--- index.jsx
+++ index.jsx
@@ -1,16 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './App.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./app.jsx";
+import { HelmetProvider } from 'react-helmet-async';
+//import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <React.StrictMode>
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
+  </React.StrictMode>
 );
+
